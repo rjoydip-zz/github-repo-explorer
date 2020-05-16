03ee132c81a0c709656e410e929a137eef8bda75
rjoydip <joydipand@gmail.com>
2020-05-16 22:46:47 +0530

    fix: breadcrumbs desing

--- components/FileExplorer.tsx
+++ components/FileExplorer.tsx
@@ -216,17 +216,30 @@ const FileExplorer = ({
                 <div className="bg-gray-100 border-b border-gray-200 py-2 px-2 flex justify-between">
                   <div className="flex items-center">
                     <span className="font-medium">
-                      {path.split('/').map((ele: string) => (
-                        <span
-                          role="button"
-                          onClick={() => onBreadcumClick(path, ele)}
-                        >
-                          <b>/</b>
-                          <a className="text-blue-500" href="#">
-                            {ele}
-                          </a>
-                        </span>
-                      ))}
+                      <nav className="text-black" aria-label="Breadcrumb">
+                        <ol className="list-none inline-flex">
+                          {path.split('/').map((ele: string, key: number) => (
+                            <li className="flex items-center">
+                              <a
+                                href="#"
+                                role="button"
+                                className="text-blue-500"
+                                onClick={() => onBreadcumClick(path, ele)}
+                              >
+                                {ele}
+                              </a>
+                              <svg
+                                className="fill-current mt-1 px-1 w-4 h-4"
+                                viewBox="0 0 320 512"
+                              >
+                                {key !== (path.split('/').length-1) ? (
+                                  <path d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
+                                ) : null}
+                              </svg>
+                            </li>
+                          ))}
+                        </ol>
+                      </nav>
                     </span>
                   </div>
                 </div>
